f2d02c4b183b504da8e8cd7a51ceb55221f664fb
tyler gorley <tylergorley96@hotmail.com>
2025-10-17 06:36:03 -0400

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace stingray_feeder_app.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
